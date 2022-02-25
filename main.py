6db060a02ea76301cdbde10a06d9f34fd1c94d8e
董成栋 <48635985+DongChengdongHangZhou@users.noreply.github.com>
2022-02-26 00:23:16 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('./color.png')
+
+channel1 = img[:,:,0]
+channel2 = img[:,:,1]
+channel3 = img[:,:,2]
+
+cv2.imwrite('c1.png',channel1)
+cv2.imwrite('c2.png',channel2)
+cv2.imwrite('c3.png',channel3)
+
+rgba = cv2.cvtColor(img,cv2.COLOR_RGB2BGRA)
+# the alpha channel is periodic from 1 to 256, if the value is out of this range, 
+# it still works, with the equal effect as the corresponding value in 1~256
+for i in range(500):
+    rgba[:,:,3] = np.ones((112,112))*i
+    cv2.imwrite('./rgba/'+str(i)+'.png',rgba)
