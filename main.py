d530c887c8fd2465132422ff110901c4c9b2c949
董成栋 <48635985+DongChengdongHangZhou@users.noreply.github.com>
2022-02-26 14:53:59 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ cv2.imwrite('c2.png',channel2)
 cv2.imwrite('c3.png',channel3)
 
 rgba = cv2.cvtColor(img,cv2.COLOR_RGB2BGRA)
-# the alpha channel is periodic from 1 to 256, if the value is out of this range, 
-# it still works, with the equal effect as the corresponding value in 1~256
+# the alpha channel is periodic from 0 to 255, if the value is out of this range, 
+# it still works, with the equal effect as the corresponding value in 0~255
 for i in range(500):
     rgba[:,:,3] = np.ones((112,112))*i
     cv2.imwrite('./rgba/'+str(i)+'.png',rgba)
